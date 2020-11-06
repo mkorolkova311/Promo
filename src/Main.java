01822659b2db237b00d2f2dd108d2823a16d4abd
Margarita Korolkova <mkorolkova311@gmail.com>
2020-11-06 22:11:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int replenishment = 1400;
+        int bonus = 0;
+
+        if(replenishment>=1000) {
+             bonus = replenishment / 100;
+    }
+       System.out.println(bonus);
+
+
+}}
